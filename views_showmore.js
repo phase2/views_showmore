c4aa32004b81bbb9ed525c07cd6d3398370bd54a
techninja <james@tn42.com>
2013-09-07 00:17:00 -0700

    SN-680: Add homespun far simpler views_showmore pager plugin

--- views_showmore.js
+++ views_showmore.js
@@ -1,60 +1,65 @@
 (function ($) {
 
   $(function() {
-    // TODO: Support multiple
-    $('.view ul.pager').each(function(){
+    $('.view ul.pager-showmore').each(function(){
       var $pagerElement = $(this);
-      var $parentView = $pagerElement.parents('.view');
+      var $parentElement = $pagerElement.parents('.view');
+
+      var $pagerLink = $('a', $pagerElement);
+      var pagerText = $pagerLink.text();
+
+      // Clear out default and recreate link
+      $pagerLink.remove();
+      $pagerLink = $('<a>').attr('href', '#').text(pagerText).data('nextpage', 1);
+
+      $('li', $pagerElement).append($pagerLink);
 
       // Get DOM id of view
-      var c = $parentView.attr('class');
+      var c = $parentElement.attr('class');
       var domID = c.substr(c.indexOf('view-dom-id-')+12).split(' ')[0];
 
       var view = Drupal.views.instances['views_dom_id:' + domID];
 
+      // Bind click event
+      $pagerLink.click(function(e){
+        e.preventDefault();
+        $(this).text('Loading...');
 
-      $pagerElement.html(''); // Clear it out
+        var submitData = view.pagerAjax.submit;
+        submitData.page = $(this).data('nextpage');
 
-      $('<li>').append(
-        $('<a>').attr('href', '#').click(function(e){
-          e.preventDefault();
-          $(this).text('Loading...');
+        $.post('/views/ajax', submitData,
+          function(data) {
+            for (var i in data) {
+              if (data[i].command == 'insert') {
+                var $d = $(data[i].data);
 
-          var submitData = view.pagerAjax.submit;
-          submitData.page = $(this).data('nextpage');
+                // Append and animate the elements
+                var $elems = $('.view-content:last', $d).contents();
+                $elems.addClass('newcontent');
+                $('.view-content:last', $parentElement).append($elems);
+                $('.view-content .newcontent', $parentElement).hide().slideDown('slow', function(){
+                  $(this).removeClass('newcontent');
+                });
 
-          $.post('/views/ajax', submitData,
-            function(data) {
-              for (var i in data) {
-                if (data[i].command == 'insert') {
-                  var $d = $(data[i].data);
-                  var $pagerLink = $('.sn-viewmore-pager');
+                // Figure out current and max pages
+                var $u = $('ul.pager-showmore', $d);
+                var pageCurrent = $u.attr('current');
+                var pageMax = $u.attr('max');
 
-                  // Append and animate the elements
-                  var $elems = $('.view-content:last', $d).contents();
-                  $elems.addClass('newcontent');
-                  $('.view-content:last').append($elems);
-                  $('.view-content .newcontent').hide().slideDown('slow', function(){
-                    $(this).removeClass('newcontent');
-                  });
-
-                  // Figure out next page, if any
-                  var pages = $('.pager-current', $d).text().split(' of ');
-
-                  // We're on the last page! remove the pager
-                  if (pages[0] == pages[1]) {
-                    $pagerElement.remove();
-                  } else { // Fill in the next page and reset the text
-                    $pagerLink
-                      .data('nextpage', parseInt(pages[0]))
-                      .text('Show More');
-                  } // End if/else last page
-                } // End if Insert
-              } // End Loop over commands
-            } // End success handler function
+                // We're on the last page! remove the pager
+                if (pageCurrent == pageMax) {
+                  $pagerElement.remove();
+                } else { // Fill in the next page and reset the text
+                  $pagerLink
+                    .data('nextpage', parseInt(pageCurrent))
+                    .text(pagerText);
+                } // End if/else last page
+              } // End if Insert
+            } // End Loop over commands
+          } // End success handler function
         );
-        }).data('nextpage', 1).addClass('sn-viewmore-pager').text('Show More')
-      ).appendTo($pagerElement);
+      });
     });
   });
 
